e98ce8608a122a2e66234143ccba5cadd05b757e
ZofiaSchneider <62666056+ZofiaSchneider@users.noreply.github.com>
2020-06-07 20:30:42 +0200

    Update otsu_threshold.py

--- otsu_threshold.py
+++ otsu_threshold.py
@@ -12,8 +12,6 @@ def otsu_threshold(image):
         mi1 = np.sum(np.array([i for i in range(t)])*p[:t])/om1
         mi2 = np.sum(np.array([i for i in range(t,256)])*p[t:])/om2
         sigma_B = om1*om2*np.power(mi2-mi1,2)
-#        if t==1:
-#            sigma_B_max = sigma_B
         if sigma_B > sigma_B_max:
             sigma_B_max = sigma_B
             threshold = t/256
