5b08b9218b8857ad5dd758de4bbb22c114f38269
NowosinskaNatalia <64729082+NowosinskaNatalia@users.noreply.github.com>
2020-06-22 00:27:18 +0200

    Add files via upload

--- double_threshold.py
+++ double_threshold.py
@@ -2,6 +2,16 @@ from otsu_threshold import otsu_threshold
 import numpy as np
 
 def double_threshold(image):
+    """
+    This function proceeds a double threshold algorithm on the image. 
+    The thresholds are calculated using Otsu method.
+    
+    Arguments:
+    image - an image to be processed
+    
+    Returns:
+    tab - an thresholded image
+    """
     upper = otsu_threshold(image[270:420,100:200])
     lower = upper/1.05
     tab = np.where(image>lower,0.5,0)
--- gaussian_smoothing.py
+++ gaussian_smoothing.py
@@ -2,10 +2,22 @@ import numpy as np
 from scipy import signal
 
 def gaussian_smoothing(image, sigma, size):
+    """
+    This function filters an image using Gaussian filter.
+    
+    ArgumentsL
+    image - image to filter
+    sigma - sigma parameter of Gaussian filter
+    size - size of a filter
+    
+    Returns:
+    im_f - filtered image
+    """
     center = size/2
     kernel = np.zeros((size,size))
     for i in range(size):
        for j in range(size):
           diff = np.sqrt((i-center)**2+(j-center)**2)
           kernel[i,j] = (1/(2*3.14*sigma))*np.exp(-(diff**2)/(2*sigma**2))
-    return signal.convolve2d(image, kernel/np.sum(kernel))
+    im_f = signal.convolve2d(image, kernel/np.sum(kernel))
+    return im_f
